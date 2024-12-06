b3d10c480765b50587d0db1eeec5b15f63a82e8d
ZOHOUN-AMEDEE <136507229+ZOHOUN-AMEDEE@users.noreply.github.com>
2024-12-06 11:50:47 +0100

    Update middlewares.py

--- infomaniak_events/infomaniak_events/middlewares.py
+++ infomaniak_events/infomaniak_events/middlewares.py
@@ -7,20 +7,25 @@ import time
 class SeleniumMiddleware:
     def __init__(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")  
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--no-sandbox")
-        
-        
-        service = Service('C:/Users/DAVIDO LAPTOP/Downloads/chromedriver-win32/chromedriver.exe')
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+
+       
+        self.service = Service('C:/Users/DAVIDO LAPTOP/Downloads/chromedriver-win32/chromedriver.exe')
+        self.driver = webdriver.Chrome(service=self.service, options=chrome_options)
 
     def process_request(self, request, spider):
         self.driver.get(request.url)
         time.sleep(5)  
+        body = self.driver.page_source
+
+        
+        request.meta["driver"] = self.driver
+
         return HtmlResponse(
             self.driver.current_url,
-            body=self.driver.page_source,
+            body=body,
             encoding='utf-8',
             request=request
         )
